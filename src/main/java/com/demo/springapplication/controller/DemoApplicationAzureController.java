97d5306e298f7a78efd7833d11c6a375e987b720
Jedsadaporn Puttakosai <toey2543.tt@gmail.com>
2023-08-04 13:42:33 +0700

    add application insight add pom and intrumentation-key

--- src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
+++ src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
@@ -5,7 +5,10 @@ package com.demo.springapplication.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,5 +21,13 @@ public class DemoApplicationAzureController {
     public String runApplicationAzure(){
         log.info("Get Mapping");
         return "Springboot Application deployed Azure SuccessFully!!";
+    }
+
+    @GetMapping("/greet/{name}")
+    public ResponseEntity<String> SayHello(
+            @PathVariable("name") String name
+    ){
+        String message = "Hello " + name;
+        return new ResponseEntity<String>(message, HttpStatus.OK);
     }
 }
