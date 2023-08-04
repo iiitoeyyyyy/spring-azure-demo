ad7a705cf4c6a091751134fe6e91f01496c5568b
Jedsadaporn Puttakosai <toey2543.tt@gmail.com>
2023-08-04 16:51:10 +0700

    add Model request Header and body

--- src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
+++ src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
@@ -3,13 +3,13 @@ package com.demo.springapplication.controller;
 
 
 
+import com.demo.springapplication.model.User;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -23,13 +23,16 @@ public class DemoApplicationAzureController {
         return "Springboot Application deployed Azure SuccessFully!!";
     }
 
-    @GetMapping("/greet/{name}")
+    @PostMapping("/greet/{name}")
     public ResponseEntity<String> SayHello(
-            @PathVariable("name") String name
+            @PathVariable("name") String name,
+            @RequestHeader(value="userId") String userId,
+            @RequestBody User user
     ){
-        String message = "Hello " + name;
-        log.info("Hello Message");
+        String message = "Hello " + name+ " " + userId+ " " + user.getFirstName()+ " " + user.getLastName()+ " " +user.getAge();
         log.info("info2");
+        log.info("Hello Message");
+        log.info("info3");
         log.debug("Debug");
         log.warn("warning");
         log.error("error");
