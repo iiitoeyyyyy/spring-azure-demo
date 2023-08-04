f6f2f73af0caf0a84292f9d62d1fa44f517e47ac
Jedsadaporn Puttakosai <toey2543.tt@gmail.com>
2023-08-04 13:16:14 +0700

    add logger

--- src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
+++ src/main/java/com/demo/springapplication/controller/DemoApplicationAzureController.java
@@ -1,13 +1,22 @@
 package com.demo.springapplication.controller;
 
+
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@Slf4j
 public class DemoApplicationAzureController {
+
 
     @GetMapping("/azure")
     public String runApplicationAzure(){
+        log.info("Get Mapping");
         return "Springboot Application deployed Azure SuccessFully!!";
     }
 }
